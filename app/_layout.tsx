970befbe4b279f0305aa72e9b8f77cde4c7bd8b0
Sean <sg280@st-andrews.ac.uk>
2023-11-02 23:53:56 +0000

    Add bold Jost font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import {
 	useFonts,
 	DelaGothicOne_400Regular,
 } from "@expo-google-fonts/dela-gothic-one";
-import { Jost_400Regular } from "@expo-google-fonts/jost";
+import { Jost_400Regular, Jost_500Medium } from "@expo-google-fonts/jost";
 
 import { Stack } from "expo-router";
 
@@ -11,6 +11,7 @@ const Layout = () => {
 	let [fontsLoaded] = useFonts({
 		DelaGothicOne_400Regular,
 		Jost_400Regular,
+		Jost_500Medium,
 	});
 
 	if (!fontsLoaded) {
