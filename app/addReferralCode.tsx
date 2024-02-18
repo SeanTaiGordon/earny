113969f291d112dd3e9fef5a969119cd7f95d1f6
Sean <sg280@st-andrews.ac.uk>
2024-02-18 17:19:00 +0000

    Add scroll view to referral code page

--- app/addReferralCode.tsx
+++ app/addReferralCode.tsx
@@ -1,6 +1,5 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import {
-	FormCheckboxLabelInput,
 	FormTextInput,
 	KeyboardDismissableView,
 	MainButton,
@@ -12,7 +11,8 @@ import {
 } from "../components";
 import styled from "styled-components/native";
 import { router, useLocalSearchParams } from "expo-router";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const AddReferralCode = () => {
 	const { emoji, color, name, email, date } = useLocalSearchParams();
@@ -21,39 +21,44 @@ const AddReferralCode = () => {
 	return (
 		<ScreenContainer>
 			<KeyboardDismissableView>
-				<PaddedContainer>
-					<NavBackButton onPress={() => router.back()} />
-					<Title>👯 Referral code </Title>
-					<Subtitle>
-						If your friend has pointed you our way, enter their referral code.
-					</Subtitle>
+				<KeyboardAwareScrollView>
+					<ScrollView showsVerticalScrollIndicator={false}>
+						<PaddedContainer>
+							<NavBackButton onPress={() => router.back()} />
+							<Title>👯 Referral code </Title>
+							<Subtitle>
+								If your friend has pointed you our way, enter their referral
+								code.
+							</Subtitle>
 
-					<View>
-						<FormTextInput
-							placeholder="Referral code"
-							onChangeText={setReferralCode}
-						/>
-					</View>
+							<View>
+								<FormTextInput
+									placeholder="Referral code"
+									onChangeText={setReferralCode}
+								/>
+							</View>
 
-					<MainButtonContainer>
-						<MainButton
-							text="Confirm"
-							onPress={() => {
-								router.push({
-									pathname: "/phone_auth/phoneNumberRequest",
-									params: {
-										emoji,
-										color,
-										name,
-										email,
-										date,
-										referralCode,
-									},
-								});
-							}}
-						/>
-					</MainButtonContainer>
-				</PaddedContainer>
+							<MainButtonContainer>
+								<MainButton
+									text="Confirm"
+									onPress={() => {
+										router.push({
+											pathname: "/phone_auth/phoneNumberRequest",
+											params: {
+												emoji,
+												color,
+												name,
+												email,
+												date,
+												referralCode,
+											},
+										});
+									}}
+								/>
+							</MainButtonContainer>
+						</PaddedContainer>
+					</ScrollView>
+				</KeyboardAwareScrollView>
 			</KeyboardDismissableView>
 		</ScreenContainer>
 	);
